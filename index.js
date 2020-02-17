736222065260a8f3766bc8c6839aedf2406ff09a
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-02-16 22:39:23 -0500

    streach done

--- index.js
+++ index.js
@@ -327,9 +327,19 @@ const argTimesTwo = (num) => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odo) {
+  let odometer = {
+    odometer : odo,
+    drive : function(dist){
+      this.odometer = this.odometer + dist;
+      return this.odometer;
+    }
+  }
+
+  return odometer;
 }
+
+
 
 /// ////// END OF CHALLENGE /////////
 /// ////// END OF CHALLENGE /////////
